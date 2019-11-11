83f166c1580be8cd83db1778b5b75a52a711beb3
Azmol-Miah <azmol@azmolmiah.com>
2019-11-11 20:59:19 +0000

    addeed the brief about section

--- src/components/Layout/HomePage/BriefAbout.js
+++ src/components/Layout/HomePage/BriefAbout.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import Box from '@material-ui/core/Box';
-import Divider from '@material-ui/core/Divider';
-import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 
-const useStyles = makeStyles(theme => ({
-  root: {}
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import RoomOutlinedIcon from '@material-ui/icons/RoomOutlined';
+import CastForEducationOutlinedIcon from '@material-ui/icons/CastForEducationOutlined';
+
+const useStyles = makeStyles(() => ({
+  root: {},
+  img: {
+    width: '100%',
+    height: '100%'
+  },
+  font: {
+    fontSize: '2rem'
+  },
+  icon: {
+    fontSize: '3rem',
+    color: '#253a52'
+  },
+  studyOptions: {
+    padding: '1rem'
+  }
 }));
 
 const BriefAbout = () => {
@@ -12,15 +29,54 @@ const BriefAbout = () => {
 
   return (
     <div className={classes.root}>
-      <Box display='flex' flexDirection='row'>
-        <Box justifyContent='center'>
-          <h3>Madinah Arabic Tuiton</h3>
-        </Box>
-        <Divider orientation='vertical' />
-        <Box justifyContent='center'>
-          <h3>Madinah Arabic Tuiton</h3>
-        </Box>
-      </Box>
+      <Container maxWidth='md'>
+        <Grid container justify='center' alignItems='center'>
+          <Grid>
+            <h3>
+              Madinah Arabic Tuiton | Qualified Teachers | Comprehensive Study
+              Programme | Adults {'&'} Children
+            </h3>
+          </Grid>
+          <Grid container direction='row' justify='space-between'>
+            <Grid item lg={6} className={classes.studyOptions}>
+              <Grid container justify='flex-end' alignItems='center'>
+                <Grid>
+                  <RoomOutlinedIcon className={classes.icon} />
+                </Grid>
+                <Grid>
+                  <div className={classes.font}>In Class Tuition</div>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item lg={6} className={classes.studyOptions}>
+              <Grid container justify='flex-start' alignItems='center'>
+                <Grid>
+                  <CastForEducationOutlinedIcon className={classes.icon} />
+                </Grid>
+                <Grid>
+                  <div className={classes.font}>On Screen Tuiton</div>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid container direction='row' justify='center' alignItems='center'>
+            <Grid item lg={9}>
+              <p>
+                Our tuition has been on offer at a localised level in and around
+                Nelson, U.K. during the last 10 years between 2009-2019,
+                however, now for the first time, we are opening up classes to
+                the public at large via online platforms such as Skype {'&'}{' '}
+                Zoom.
+              </p>
+            </Grid>
+            <Grid item lg={3}>
+              <Grid container justify='flex-end'>
+                <img className={classes.img} src='/img/badge.png' alt='badge' />
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 };
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,16 +1,13 @@
 import React, { Fragment } from 'react';
 
 import Header from '../Layout/HomePage/Header';
-import Container from '@material-ui/core/Container';
 import BriefAbout from '../Layout/HomePage/BriefAbout';
 
 const HomePage = () => {
   return (
     <Fragment>
       <Header />
-      <Container maxWidth='md'>
-        <BriefAbout />
-      </Container>
+      <BriefAbout />
     </Fragment>
   );
 };
